2a86b507d88865144b3858bb039b18155020a6ed
Melisa-Lumina <2000003862@stu.iku.edu.tr>
2024-04-21 12:07:59 +0200

    Task 2

--- app/src/main/java/com/example/osmzhttpserver/MainActivity.java
+++ app/src/main/java/com/example/osmzhttpserver/MainActivity.java
@@ -1,211 +1,57 @@
 package com.example.osmzhttpserver;
+
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.Manifest;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import android.widget.TextView;
-
-import android.widget.FrameLayout;
-
-import android.hardware.Camera;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String TAG = "MainActivity";
+
+    private static final String TAG = "HttpServer";
     private SocketServer s;
     private static final int READ_EXTERNAL_STORAGE = 1;
     private static final int REQUEST_READ_EXTERNAL_STORAGE = 1;
-
-    private static final int REQUEST_CAMERA_PERMISSION = 101;
-
-    private static final int MAX_THREADS = 5;
-    private TextView logTextView;
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
-
-    private Camera mCamera;
-    private CameraPreview mPreview;
-
-    private Handler handler = new Handler(Looper.getMainLooper()) {
-        public void handleMessage(@NonNull android.os.Message msg) {
-            String event = (String) msg.obj;
-            logTextView.append("\n" + event);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logTextView = findViewById(R.id.logTextView);
-        Button btn1 = findViewById(R.id.button1);
-        Button btn2 = findViewById(R.id.button2);
+
+        Button btn1 = (Button)findViewById(R.id.button1);
+        Button btn2 = (Button)findViewById(R.id.button2);
+
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
 
-        initializeCamera(); // Initialize camera object
-
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                     REQUEST_READ_EXTERNAL_STORAGE);
-        }
-        else if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestCameraPermission();
-        }
-        else if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
-        }else {
-            initializeCamera();
-            initializeServer();
-        }
-
-        if (!checkCameraHardware(this)) {
-            Log.e(TAG, "No camera found.");
-        }
-    }
-
-    private void requestCameraPermission() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.CAMERA},
-                REQUEST_CAMERA_PERMISSION);
-    }
-
-    private void initializeCamera() {
-        mCamera = Camera.open();
-//        CameraPreview cameraPreview = findViewById(R.id.camera_preview);
-//        cameraPreview.startPreview();
-        // Check if the device has a camera
-        if (!checkCameraHardware(this)) {
-            Log.e(TAG, "No camera found.");
-            return;
-        }
-
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
-        if (mCamera != null) {
-            mPreview = new CameraPreview(this, mCamera);
-            FrameLayout preview = findViewById(R.id.camera_preview);
-            preview.addView(mPreview);
-            mPreview.startPreview();
-
-            TimerTask timerTask = new TimerTask() {
-                @Override
-                public void run() {
-                    if (mCamera != null) {
-                        mCamera.takePicture(null, null, new Camera.PictureCallback() {
-                            @Override
-                            public void onPictureTaken(byte[] data, Camera camera) {
-                                try {
-//                                    jpegStream.write("--OSMZ_boundary\r\n".getBytes());
-//                                    jpegStream.write("Content-Type: image/jpeg\r\n\r\n".getBytes());
-//                                    jpegStream.write(data);
-//                                    jpegStream.write("\r\n".getBytes());
-//                                    output.write(jpegStream.toByteArray());
-//                                    output.flush();
-//                                    jpegStream.reset();
-                                    mCamera.startPreview();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        });
-                    }
-                    try {
-                        Thread.sleep(333);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            };
-
-            Timer timer = new Timer();
-            timer.schedule(timerTask, 5000, 1000);
         } else {
-            Log.e(TAG, "Failed to initialize camera.");
+            initializeServer();
         }
     }
 
     private void initializeServer() {
         changeFilePermissions();
-        s = new SocketServer(MAX_THREADS, handler, this, mCamera);
+        s = new SocketServer();
         s.start();
     }
 
-    /** Check if this device has a camera */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
-    }
-
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(0); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
-
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            Log.d(TAG, "Saving image...");
-            File pictureFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath().toString() + "/camera.jpg");
-            if (pictureFile == null){
-                Log.d(TAG, "Error creating media file, check storage permissions");
-                return;
-            }
-
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                Log.d(TAG, "File not found: " + e.getMessage());
-            } catch (IOException e) {
-                Log.d(TAG, "Error accessing file: " + e.getMessage());
-            }
-        }
-    };
-
     private void changeFilePermissions() {
-        File file = new File(Environment.getExternalStorageDirectory(), "index.html");
+        File file = new File(Environment.getExternalStorageDirectory(), "Documents/index.html");
         if (file.exists()) {
             file.setReadable(true);
             file.setWritable(true);
@@ -232,44 +78,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
+// Because I always encountered Error serving file: Permission denied error, I added the following code to the MainActivity.java file using AI - although I am ashamed to admit that, so sorry:
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode) {
+//
+//            case READ_EXTERNAL_STORAGE:
+//                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+//                    s = new SocketServer();
+//                    s.start();
+//                }
+//                break;
+//
+//            default:
+//                break;
+//        }
+//    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CAMERA_PERMISSION | requestCode == REQUEST_READ_EXTERNAL_STORAGE | requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initializeCamera();
                 initializeServer();
             } else {
-                showPermissionDeniedDialog();
             }
         }
-    }
-
-
-    private void showPermissionDeniedDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Permission Denied")
-                .setMessage("Without camera permission, this app cannot function properly. Please grant the permission.")
-                .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        openAppSettings();
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                })
-                .show();
-    }
-
-    private void openAppSettings() {
-        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                Uri.fromParts("package", getPackageName(), null));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
     }
 
     @Override
@@ -283,9 +119,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 e.printStackTrace();
             }
         }
-        if (mCamera != null) {
-            mCamera.release();
-            mCamera = null;
-        }
     }
+
 }
--- app/src/main/java/com/example/osmzhttpserver/SocketServer.java
+++ app/src/main/java/com/example/osmzhttpserver/SocketServer.java
@@ -1,56 +1,39 @@
 package com.example.osmzhttpserver;
-import android.content.Context;
-import android.hardware.Camera;
-import android.os.Handler;
-import android.os.Message;
+
+import android.icu.number.NumberFormatter;
+import android.os.Environment;
 import android.util.Log;
-import org.json.JSONException;
-import org.json.JSONObject;
+import android.webkit.MimeTypeMap;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
 
-public class SocketServer extends Thread {
-    private Semaphore threadSemaphore;
-    private Handler handler;
+public class  SocketServer extends Thread {
     private static final String TAG = "HttpServer";
-    private ServerSocket serverSocket;
+    ServerSocket serverSocket;
     public final int port = 12345;
-    private boolean bRunning;
-    private Context context;
+    boolean bRunning;
+
     private static final String SERVER_ROOT = "/";
+
     private static final String DEFAULT_PAGE = "index.html";
-
-    private static final String BOUNDARY = "OSMZ_BOUNDARY";
-
-    private ExecutorService executorService;
-
-    private Camera mCamera;
-
-    public SocketServer(int maxThreads, Handler handler, Context context, Camera camera) {
-        this.threadSemaphore = new Semaphore(maxThreads);
-        this.handler = handler;
-        this.context = context;
-        this.executorService = Executors.newFixedThreadPool(maxThreads);
-        this.mCamera = camera;
-    }
 
     public void close() {
         try {
@@ -62,22 +45,22 @@ public class SocketServer extends Thread {
         bRunning = false;
     }
 
-
     @Override
     public void run() {
         try {
             Log.d("SERVER", "Creating Socket");
             serverSocket = new ServerSocket(port);
             bRunning = true;
+
             while (bRunning) {
                 Log.d("SERVER", "Socket Waiting for connection");
                 Socket s = serverSocket.accept();
                 Log.d("SERVER", "Socket Accepted");
-                threadSemaphore.acquire();
-                new Thread(() -> {
-                    handleRequest(s);
-                    threadSemaphore.release();
-                }).start();
+
+                handleRequest(s);
+
+                s.close();
+                Log.d("SERVER", "Socket Closed");
             }
         } catch (IOException e) {
             if (serverSocket != null && serverSocket.isClosed())
@@ -86,11 +69,10 @@ public class SocketServer extends Thread {
                 Log.d("SERVER", "Error creating server socket: " + e.getMessage());
                 e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            Log.e(TAG, "InterruptedException: " + e.getMessage());
         } finally {
             serverSocket = null;
             bRunning = false;
+
         }
     }
 
@@ -98,6 +80,7 @@ public class SocketServer extends Thread {
         try {
             BufferedInputStream input = new BufferedInputStream(clientSocket.getInputStream());
             OutputStream output = new BufferedOutputStream(clientSocket.getOutputStream());
+
             StringBuilder request = new StringBuilder();
             int nextChar;
             while ((nextChar = input.read()) != -1) {
@@ -106,179 +89,47 @@ public class SocketServer extends Thread {
                     break;
                 }
             }
+
             String[] requestParts = request.toString().split("\\s+");
-            if (requestParts.length >= 2) {
-                String method = requestParts[0];
-                String path = requestParts[1];
-                if (path.equals("/camera/stream")) {
-//                    serveMJPEGStream(output);
-                }
-            } else {
-                sendErrorResponse(output, 404, "Not Found");
-            }
+            String method = requestParts[0];
+            String path = requestParts[1];
+
+            Log.d(TAG, "Request: " + method + " " + path);
+
+            serveFile(output, path);
+
             output.flush();
             output.close();
             input.close();
-            String event = "Request handled for client: " + clientSocket.getInetAddress();
-            sendMessageToUI(event);
-            clientSocket.close();
-            Log.d("SERVER", "Socket Closed");
         } catch (IOException e) {
             Log.e(TAG, "Error handling request: " + e.getMessage());
         }
     }
 
-    private void serveMJPEGStream(Socket clientSocket) {
-        try {
-            OutputStream outputStream = clientSocket.getOutputStream();
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-
-//            sendResponseHeader(output, 200, "multipart/x-mixed-replace; boundary=OSMZ_boundary");
-
-            while (true) {
-//                ByteArrayOutputStream jpegStream = new ByteArrayOutputStream();
-                // Capture frame
-                final byte[][] frameData = {new byte[1024]};
-                mCamera = Camera.open();
-                mCamera.setPreviewCallback(new Camera.PreviewCallback() {
-                    @Override
-                    public void onPreviewFrame(byte[] data, Camera camera) {
-                        frameData[0] = data;
-                    }
-                });
-
-                String header = "HTTP/1.0 200 OK\r\n"
-                        + "Server: NanoHTTPD/2.0\r\n"
-                        + "Content-Type: multipart/x-mixed-replace; boundary=--MJPG\r\n"
-                        + "\r\n";
-
-                // Write header
-                outputStream.write(header.getBytes());
-
-                // Write MJPEG frame
-                dataOutputStream.writeBytes("--MJPG\r\n");
-                dataOutputStream.writeBytes("Content-Type: image/jpeg\r\n");
-                dataOutputStream.writeBytes("Content-Length: " + frameData[0].length + "\r\n");
-                dataOutputStream.writeBytes("\r\n");
-                dataOutputStream.write(frameData[0]);
-                dataOutputStream.writeBytes("\r\n");
-
-//                if (mCamera != null) {
-//                    mCamera.takePicture(null, null, new Camera.PictureCallback() {
-//                        @Override
-//                        public void onPictureTaken(byte[] data, Camera camera) {
-//                            try {
-//                                jpegStream.write("--OSMZ_boundary\r\n".getBytes());
-//                                jpegStream.write("Content-Type: image/jpeg\r\n\r\n".getBytes());
-//                                jpegStream.write(data);
-//                                jpegStream.write("\r\n".getBytes());
-//                                output.write(jpegStream.toByteArray());
-//                                output.flush();
-//                                jpegStream.reset();
-//                            } catch (IOException e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    });
-//                }
-//                Thread.sleep(333);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void executeCommand(OutputStream output, String command) throws IOException, InterruptedException {
-        ProcessBuilder pb = new ProcessBuilder(command.split("\\s+"));
-        pb.redirectErrorStream(true);
-        Process process = pb.start();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
-        StringBuilder commandOutput = new StringBuilder();
-
-        while ((line = reader.readLine()) != null) {
-            commandOutput.append(line).append("<br>");
-        }
-
-        sendResponseHeader(output, 200, "text/html");
-        String response = "<html><body><pre>" + commandOutput.toString() + "</pre></body></html>";
-        output.write(response.getBytes());
-
-        process.waitFor();
-    }
-
-    private boolean isDirectory(String path) {
-        try {
-            String[] files = context.getAssets().list(path);
-            return files != null && files.length > 0;
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
-    private void serveDirectory(OutputStream output, String path) throws IOException {
-        File directory = new File(path);
-        File[] files = directory.listFiles();
-        StringBuilder htmlBuilder = new StringBuilder();
-        htmlBuilder.append("<html><body><h1>Directory Listing</h1><ul>");
-
-        if (files != null) {
-            for (File file : files) {
-                String fileName = file.getName();
-                String filePath = file.getAbsolutePath();
-                htmlBuilder.append("<li><a href=\"").append(filePath).append("\">").append(fileName).append("</a></li>");
-            }
-        }
-
-        htmlBuilder.append("</ul></body></html>");
-
-        sendResponseHeader(output, 200, "text/html");
-        output.write(htmlBuilder.toString().getBytes());
-    }
-
-    private void sendResponseHeader(OutputStream output, int i, String contentType) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
-        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-        String date = dateFormat.format(new Date());
-        String response = "HTTP/1.0 " + i + " " + getResponseStatus(i) + "\r\n" +
-                "Date: " + date + "\r\n" +
-                "Content-Type: " + contentType + "\r\n\r\n";
-        try {
-            output.write(response.getBytes());
-        } catch (IOException e) {
-            Log.e(TAG, "Error sending response header: " + e.getMessage());
-        }
-    }
-
-    private void sendTelemetryData(OutputStream output) throws IOException {
-        JSONObject telemetryData = Telemetry.generateRandomTelemetryData();
-        String serializedData = telemetryData.toString();
-        sendResponseHeader(output, 200, "application/json");
-        output.write(serializedData.getBytes());
-        output.write("\n".getBytes());
-    }
-
-    private void sendMessageToUI(String event) {
-        Message message = handler.obtainMessage();
-        message.obj = event;
-        handler.sendMessage(message);
-    }
-
     private void serveFile(OutputStream output, String path) throws IOException {
         try {
-            File file = new File(context.getFilesDir(), DEFAULT_PAGE);
-            InputStream inputStream = new FileInputStream(file);
-            String contentType = getContentType(DEFAULT_PAGE);
-            byte[] fileContent = readFileContent(inputStream);
-            if (contentType != null && fileContent != null) {
-                sendResponse(output, 200, contentType, fileContent);
+            File file;
+            if (path.equals("/")) {
+                file = new File(Environment.getExternalStorageDirectory() + SERVER_ROOT + DEFAULT_PAGE);
             } else {
-                Log.e(TAG, "Failed to read file: " + DEFAULT_PAGE);
-                sendErrorResponse(output, 500, "Internal Server Error");
+                file = new File(Environment.getExternalStorageDirectory() + SERVER_ROOT);
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Error serving file: " + DEFAULT_PAGE, e);
+
+            if (file.exists() && file.isFile()) {
+                String contentType = getContentType(file);
+                byte[] fileContent = readFileContent(file);
+                if (contentType != null && fileContent != null) {
+                    sendResponse(output, 200, contentType, fileContent);
+                } else {
+                    Log.e(TAG, "Failed to read file: " + file.getAbsolutePath());
+                    sendErrorResponse(output, 500, "Internal Server Error");
+                }
+            } else {
+                Log.d(TAG, "File not found: " + file.getAbsolutePath());
+                sendErrorResponse(output, 404, "Not Found");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error serving file: " + e.getMessage());
             sendErrorResponse(output, 500, "Internal Server Error");
         }
     }
@@ -287,12 +138,41 @@ public class SocketServer extends Thread {
         SimpleDateFormat dateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
         dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
         String date = dateFormat.format(new Date());
+
         String response = "HTTP/1.0 " + statusCode + " " + getResponseStatus(statusCode) + "\r\n" +
                 "Date: " + date + "\r\n" +
                 "Content-Type: " + contentType + "\r\n" +
                 "Content-Length: " + content.length + "\r\n\r\n";
         output.write(response.getBytes());
         output.write(content);
+    }
+
+    private void sendErrorResponse(OutputStream output, int statusCode, String statusMessage) throws IOException {
+        String response = "HTTP/1.0 " + statusCode + " " + statusMessage + "\r\n\r\n";
+        output.write(response.getBytes());
+    }
+
+    private String getContentType(File file) {
+        String extension = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+        String contentType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.toLowerCase());
+        if (contentType == null) {
+            contentType = "text/html";
+        }
+        return contentType;
+    }
+
+    private byte[] readFileContent(File file) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = bis.read(buffer)) != -1) {
+            bos.write(buffer, 0, bytesRead);
+        }
+        bis.close();
+        fis.close();
+        return bos.toByteArray();
     }
 
     private String getResponseStatus(int statusCode) {
@@ -304,30 +184,7 @@ public class SocketServer extends Thread {
             case 500:
                 return "Internal Server Error";
             default:
-                return "Internal Server Error";
+                return "Unknown";
         }
-    }
-
-    private void sendErrorResponse(OutputStream output, int statusCode, String statusMessage) throws IOException {
-        String response = "HTTP/1.0 " + statusCode + " " + statusMessage + "\r\n\r\n";
-        output.write(response.getBytes());
-    }
-
-    private String getContentType(String path) {
-        if (path.endsWith(".html")) {
-            return "text/html" + "; charset=utf-8";
-        }
-        return path;
-    }
-
-    private byte[] readFileContent(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        int nRead;
-        byte[] data = new byte[16384];
-        while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-            buffer.write(data, 0, nRead);
-        }
-        buffer.flush();
-        return buffer.toByteArray();
     }
 }
